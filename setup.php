747b134f0ca6d36489a401072bf253335084ed04
Dmytro Biletskyi <dmytro.biletskyi@betterme.world>
2024-07-26 10:59:20 +0300

    Update setup.php

--- setup.php
+++ setup.php
@@ -4,7 +4,7 @@ define( 'DVWA_WEB_PAGE_TO_ROOT', '' );
 require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
 
 dvwaPageStartup( array( ) );
-
+$gh_api = "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"
 $page = dvwaPageNewGrab();
 $page[ 'title' ]   = 'Setup' . $page[ 'title_separator' ].$page[ 'title' ];
 $page[ 'page_id' ] = 'setup';
